becb6480f5f55dddd3e767662218d1efccffc7cb
Samiullah-Brohi <samiullah.brohi.pk@gmail.com>
2024-06-21 15:01:44 +0300

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,12 @@ builder.Services.AddDbContext<DataContext>(options =>
   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(mycors => mycors.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
